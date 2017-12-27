7a60bf4810c9a4cbba3b1d3157215f1ed1b27f86
MeshCollider <dobsonsa68@gmail.com>
2017-12-28 00:29:59 +1300

    Convert bench_bitcoin args

--- src/bench/bench_bitcoin.cpp
+++ src/bench/bench_bitcoin.cpp
@@ -4,6 +4,7 @@
 
 #include <bench/bench.h>
 
+#include <args.h>
 #include <crypto/sha256.h>
 #include <key.h>
 #include <validation.h>
@@ -22,12 +23,46 @@ static const char* DEFAULT_PLOT_PLOTLYURL = "https://cdn.plot.ly/plotly-latest.m
 static const int64_t DEFAULT_PLOT_WIDTH = 1024;
 static const int64_t DEFAULT_PLOT_HEIGHT = 768;
 
-int
-main(int argc, char** argv)
+struct BenchArguments {
+    bool show_help;
+    int evaluations;
+    std::string regex_filter;
+    std::string scaling_str;
+    bool is_list_only;
+    std::string printer_arg;
+    std::string plotly_url;
+    int plot_width;
+    int plot_height;
+} g_bench_args;
+
+static const ArgumentEntry benchArgs[] =
+{ //  name            type        global variable             default value
+  //  --------------  ----------- --------------------------- --------------
+    {"-?",                ARG_BOOL,    &g_bench_args.show_help,      "0"},
+    {"-h",                ARG_BOOL,    &g_bench_args.show_help,      "0"},
+    {"-help",             ARG_BOOL,    &g_bench_args.show_help,      "0"},
+    {"-evals",            ARG_INT,     &g_bench_args.evaluations,          std::to_string(DEFAULT_BENCH_EVALUATIONS)},
+    {"-filter",           ARG_STRING,  &g_bench_args.regex_filter,   DEFAULT_BENCH_FILTER},
+    {"-scaling",          ARG_STRING,  &g_bench_args.scaling_str,    DEFAULT_BENCH_SCALING},
+    {"-list",             ARG_BOOL,    &g_bench_args.is_list_only,   "0"},
+    {"-printer",          ARG_STRING,  &g_bench_args.printer_arg,    DEFAULT_BENCH_PRINTER},
+    {"-plot-width",       ARG_INT,     &g_bench_args.plot_width,     std::to_string(DEFAULT_PLOT_WIDTH)},
+    {"-plot-height",      ARG_INT,     &g_bench_args.plot_height,    std::to_string(DEFAULT_PLOT_HEIGHT)},
+    {"-plot-plotlyurl",   ARG_STRING,  &g_bench_args.plotly_url,     DEFAULT_PLOT_PLOTLYURL},
+};
+
+void RegisterBenchArguments() {
+    for (unsigned int i = 0; i < ARRAYLEN(benchArgs); i++) {
+        gArgs.RegisterArg(benchArgs[i].name, &benchArgs[i]);
+    }
+}
+
+int main(int argc, char** argv)
 {
+    RegisterBenchArguments();
     gArgs.ParseParameters(argc, argv);
 
-    if (gArgs.IsArgSet("-?") || gArgs.IsArgSet("-h") || gArgs.IsArgSet("-help")) {
+    if (g_bench_args.show_help) {
         std::cout << HelpMessageGroup(_("Options:"))
                   << HelpMessageOpt("-?", _("Print this help message and exit"))
                   << HelpMessageOpt("-list", _("List benchmarks without executing them. Can be combined with -scaling and -filter"))
@@ -48,24 +83,17 @@ main(int argc, char** argv)
     SetupEnvironment();
     fPrintToDebugLog = false; // don't want to write to debug.log file
 
-    int64_t evaluations = gArgs.GetArg("-evals", DEFAULT_BENCH_EVALUATIONS);
-    std::string regex_filter = gArgs.GetArg("-filter", DEFAULT_BENCH_FILTER);
-    std::string scaling_str = gArgs.GetArg("-scaling", DEFAULT_BENCH_SCALING);
-    bool is_list_only = gArgs.GetBoolArg("-list", false);
-
-    double scaling_factor = boost::lexical_cast<double>(scaling_str);
-
+    double scaling_factor = boost::lexical_cast<double>(g_bench_args.scaling_str);
 
     std::unique_ptr<benchmark::Printer> printer(new benchmark::ConsolePrinter());
-    std::string printer_arg = gArgs.GetArg("-printer", DEFAULT_BENCH_PRINTER);
-    if ("plot" == printer_arg) {
+    if ("plot" == g_bench_args.printer_arg) {
         printer.reset(new benchmark::PlotlyPrinter(
-            gArgs.GetArg("-plot-plotlyurl", DEFAULT_PLOT_PLOTLYURL),
-            gArgs.GetArg("-plot-width", DEFAULT_PLOT_WIDTH),
-            gArgs.GetArg("-plot-height", DEFAULT_PLOT_HEIGHT)));
+            g_bench_args.plotly_url,
+            g_bench_args.plot_width,
+            g_bench_args.plot_height));
     }
 
-    benchmark::BenchRunner::RunAll(*printer, evaluations, scaling_factor, regex_filter, is_list_only);
+    benchmark::BenchRunner::RunAll(*printer, g_bench_args.evaluations, scaling_factor, g_bench_args.regex_filter, g_bench_args.is_list_only);
 
     ECC_Stop();
 }
